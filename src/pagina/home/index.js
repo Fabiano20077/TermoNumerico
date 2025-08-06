0d7c70801d0e6b7e0a872a187ac7831c4a22598a
fabiano <ferreirafabiano427@gmail.com>
2025-08-06 20:46:00 -0300

    alteração no visual e mecanica do termo

--- src/pagina/home/index.js
+++ src/pagina/home/index.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, Text , TextInput, Pressable ,FlatList } from 'react-native-web';
+import { View, Text, TextInput, Pressable, FlatList } from 'react-native-web';
 import styles from './style';
 import { useEffect, useState } from 'react';
 
@@ -7,86 +7,111 @@ export default function App() {
 
 
 
-    const [num,setNum] = useState([0,0,0,0]);
-    const [Lista,setLista] = useState([]);  
-   // const numero = [0,0,0,0]
+    const [num, setNum] = useState([0, 0, 0, 0]);
+    const [Lista, setLista] = useState([
+        [``, '', '', ''],
+        [``, '', '', ''],
+        [``, '', '', ''],
+        [``, '', '', ''],
+        [``, '', '', '']
+    ]);
 
-    const addArray =(num) => {
+    // const numero = [0,0,0,0]
 
+    /*  const addArray = (num) => {
+ 
+ 
+         const novoItem = {
+             valor: num
+         }
+ 
+         const updateAray = [...Lista, novoItem];
+         setLista(updateAray);
+ 
+         setNum('');
+     } */
 
-        const novoItem = {
-            valor: num
-        }
-            
-        const updateAray = [...Lista,novoItem];
-        setLista(updateAray);
+    const addLista = (num) => {
 
-        setNum('');
+        
+
     }
 
 
-  return (
-    <View style={styles.container}>
-        <View style={styles.Campo}> 
-            <View style={styles.tela}>
-                <View style={styles.numero}>
+    return (
+        <View style={styles.container}>
+            <View style={styles.Campo}>
+                <View style={styles.tela}>
+                    <View style={styles.numero}>
 
-                    <View style={styles.containerBlocos}>
-
-                    <FlatList 
-                    data={Lista}
-                    keyExtractor={(item) => item.valor}
-                    contentContainerStyle={{padding: 20}}
-                    renderItem={({item}) => (
-
-                        <View style={styles.flat}>
-                            <Text style={styles.containeNumero} >  {item.valor} </Text>
-                            <Text style={styles.containeNumero} >  {item.valor} </Text>
-                            <Text style={styles.containeNumero} >  {item.valor} </Text>
-                            <Text style={styles.containeNumero} >  {item.valor} </Text>
+                        <View>
+                            <Text style={styles.Titulo}>Termo Numerico</Text>
                         </View>
-                              
-                    )}  
-                    />
-                    
+                        <View>
+
+
+                            <FlatList
+                                data={Lista}
+                                keyExtractor={(item) => item.valor}
+                                contentContainerStyle={{ padding: 20 }}
+                                renderItem={({ item, index }) => (
+
+                                    <View style={styles.flat}>
+
+
+                                        <View style={styles.containeNumero} >
+                                            <Text style={styles.texto}> {item[0]}  </Text>
+                                        </View>
+
+                                        <View style={styles.containeNumero} >
+                                            <Text style={styles.texto}> {item[1]}  </Text>
+                                        </View>
+
+                                        <View style={styles.containeNumero} >
+                                            <Text style={styles.texto}> {item[2]}  </Text>
+                                        </View>
+
+                                        <View style={styles.containeNumero} >
+                                            <Text style={styles.texto}> {item[3]}  </Text>
+                                        </View>
+
+                                    </View>
+
+                                )}
+                            />
+
+                        </View>
                     </View>
-     {num}
+
+                    <View style={styles.containerInput}>
+
+                        <Pressable style={styles.input} onPress={addLista(1)}>
+                            <Text> 1 </Text>
+                        </Pressable>
+
+                        <Pressable style={styles.input} onPress={addLista(2)}>
+                            <Text> 2 </Text>
+                        </Pressable>
+
+                        <Pressable style={styles.input} onPress={addLista(3)}>
+                            <Text> 3 </Text>
+                        </Pressable>
+
+                        <Pressable style={styles.input} onPress={addLista(4)}>
+                            <Text> 4 </Text>
+                        </Pressable>
+
+                    </View>
                 </View>
+                <View style={styles.teclado}>
 
-                <View style={styles.containerInput}>
-
-                        <TextInput
-                        style={styles.input}
-                        
-                        onChangeText={setNum}
-                        /> 
-                        <TextInput
-                        style={styles.input}
-                        
-                        onChangeText={setNum}
-                        /> 
-                        <TextInput
-                        style={styles.input}
-                        
-                        onChangeText={setNum}
-                        /> 
-                        <TextInput
-                        style={styles.input}
-                        
-                        onChangeText={setNum}
-                        /> 
+                    <Pressable style={styles.botao} onPress={() => addArray(num)}>
+                        <Text style={styles.veri}> verificar </Text>
+                    </Pressable>
 
                 </View>
             </View>
-            <View style={styles.teclado}>
-
-                <Pressable style={styles.botao} onPress={() => addArray(num)}>
-                    <Text style={styles.veri}> verificar </Text>
-                </Pressable>
-
-            </View>
+            <StatusBar style="auto" />
         </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+    );
 }
--- src/pagina/home/style.js
+++ src/pagina/home/style.js
@@ -1,80 +1,87 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#ccc',
+  container: {
+    flex: 1,
+    backgroundColor: '#323232',
+
+  },
+
+  Campo: {
+    flex: 1,
+    backgroundColor: '#5da051ff',
+  },
+
+  tela: {
+    flex: 1,
+    borderWidth: 1
+  },
+
+  numero: {
+    flex: 1,
+    borderWidth: 1,
+    alignItems: 'center',
+
+  },
+
+  Titulo: {
+    padding: 5,
+    fontSize: 30,
+    color: 'white'
+  },
+
+  flat: {
+    flexDirection: 'row'
+  },
+
+  containeNumero: {
+    margin: 5,
+    height: 70,
+    width: 70,
+    backgroundColor: 'rgba(0,0,0,.8)',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+
+  texto: {
     
-    },
+    color: 'white',
+    fontSize: 40
+  },
 
-    Campo: {
-      flex: 1,
-      
-    },
-
-    tela:{
-      flex: 1,
-      borderWidth: 1
-    },
-
-    numero:{
-      flex: 1,
-      borderWidth:1,
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-
-    containerBlocos:{
-      width: 250,
-      height: 300,
-      borderWidth: 1,
-     
-    },
-
-    flat:{
-      flexDirection: 'row'
-    },
-
-    containeNumero:{
-      margin: 5,
-      padding: 10 ,
-      backgroundColor: 'black',
-      color: 'white',
-    },
-
-    containerInput:{
-        flex: 0.2,
-        borderWidth: 1,
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        flexDirection: 'row'
-    },
+  containerInput: {
+    flex: 0.2,
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row'
+  },
 
 
-    input:{
-      height: 50,
-      width: 50,
-      backgroundColor: 'white',
-      borderWidth: 1,
-      fontSize: 30,
-    },
+  input: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    fontSize: 30,
+  },
 
-    teclado:{
-      flex: 0.5,
-      borderWidth: 1
-    },
+  teclado: {
+    flex: 0.1,
+    borderWidth: 1
+  },
 
-    botao:{
-      flex: 1,
-      alignItems:'center',
-      justifyContent: 'center',
-      backgroundColor: 'blue',
-    },
+  botao: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+  },
 
-    veri: {
-      color: 'white',
-      fontSize: 60,
-    }
+  veri: {
+    color: 'white',
+    fontSize: 60,
+  }
 
-  });
-  
+});
